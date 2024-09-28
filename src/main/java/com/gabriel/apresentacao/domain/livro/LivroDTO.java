4182e9bd2f6c351897e835710af3fc9881ea466e
GabrieLima12 <gabrieldocouto.1207@gmail.com>
2024-09-28 00:35:19 -0300

    feat: creating a date format.

--- src/main/java/com/gabriel/apresentacao/domain/livro/LivroDTO.java
+++ src/main/java/com/gabriel/apresentacao/domain/livro/LivroDTO.java
@@ -1,10 +1,13 @@
 package com.gabriel.apresentacao.domain.livro;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDate;
 
 public record LivroDTO(
         Integer id,
         String nome,
+        @JsonFormat(pattern = "dd/MM/yyyy")
         LocalDate dataPublicacao,
         String autor,
         String categoria
